8c4e98290db44675e11b2681df7bf82aaacedd60
whkuoattaiwan <whkuoattaiwan@641bb3ea-ff26-834b-be61-8224bbf0acb5>
2010-04-29 02:05:28 +0000

    fullfill autocomplete

--- src/archive/web/js/jquery4j/jqueryui/Autocomplete.js
+++ src/archive/web/js/jquery4j/jqueryui/Autocomplete.js
@@ -18,31 +18,50 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 */
 
 jquery4j.jqueryui.Autocomplete = zk.$extends(zul.Widget,{
-	_selected : null,
-	_availableTags :[],
+	
+	$define: {		
+		selected : null,
 		
-	$define: {
-		availableTags : null,
-		selected : null
+		disabled :  function(){
+			$(this.$n('cnt')).autocomplete('option','disabled',this._disabled);
+		},	
+		delay :  function(){
+			$(this.$n('cnt')).autocomplete('option','delay',this._delay);
+		},
+		minLength :  function(){
+			$(this.$n('cnt')).autocomplete('option','minLength',this._minLength);
+		},
+		source :  function(){
+			$(this.$n('cnt')).autocomplete('option','source',this._source);
+		}		
 	},
 	
 	bind_: function () {
 		this.$supers('bind_', arguments);
 		var content = this.$n('cnt');		
 
-		//zk.log(this.$n('cnt').id);		
-		//zk.log("tags: "+this._availableTags);		
-		var wgt=this;				
-		$(content).autocomplete({
-			source : this._availableTags,
-   			select: function(event, ui) {				
-				//zk.log(ui.item.value);
+		var wgt=this;
+		var options = {
+   			select: function(event, ui) {								
 				wgt.fire('onSelection',{start: 0, end: 0,
 				selected: ui.item.value });
-			}			
-		});					
+			}
+			,source : this._source
+			,disabled: this._disabled
+			//,disabled:true  still no effect
+			,delay: this._delay
+			,minLength: this._minLength			
+		}
+						
+		$(content).autocomplete(options);					
 	},
-		
+	
+	unbind_: function () {				
+		var content = this.$n('cnt');
+		$(content).autocomplete('destroy');		
+		this.$supers('unbind_', arguments);
+	},	
+			
 	redraw: function (out) {
 		out.push('<div',this.domAttrs_(),'>');
 		out.push('<input id="',this.uuid,'-cnt" ','type="text"/>');
--- src/org/zkoss/jquery4j/jqueryui/autocomplete/Autocomplete.java
+++ src/org/zkoss/jquery4j/jqueryui/autocomplete/Autocomplete.java
@@ -17,20 +17,85 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 
 package org.zkoss.jquery4j.jqueryui.autocomplete;
 
+import org.zkoss.jquery4j.jqueryui.parameter.Mix;
 import org.zkoss.lang.Objects;
 import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.event.SelectionEvent;
 import org.zkoss.zul.impl.XulElement;
 
-public class Autocomplete extends XulElement {	
-	String _selected="";
-	String [] _availableTags = {};//{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+public class Autocomplete extends XulElement {
 
 	static {
 		addClientEvent(Autocomplete.class, Events.ON_SELECTION, CE_IMPORTANT|CE_REPEAT_IGNORE);		
 	}
+
+	//user selected value
+	private String _selected="";
+
+	private boolean _disabled = false;
 	
+	private int _delay = 300;	
+	private int _minLength = 1;
+	
+	private Mix _source = new Mix();//no default value, but never be null
+
+	public Object getSource() {
+		if(_source != null){
+			return _source.getValue();
+		}
+		return null;
+	}
+	
+	public void setSource(Object source){
+		if(_source == null) source = new Mix();
+		if(source instanceof String && ((String)source).contains(",")){
+			source = ((String)source).split(",");
+		}
+		_source.setValue(source);
+		smartUpdate("source", _source.getValue());
+	}
+	
+	public int getMinLength(){
+		return _minLength;
+	}
+	
+	public void setMinLength(int minLength){
+		if (minLength < 0 )
+			throw new UiException("Illegal minLength: "+minLength+". Range: 0 ~ ");
+		
+		if(_minLength!=minLength){
+			_minLength = minLength;
+			smartUpdate("minLength",_minLength);
+		}
+	}
+		
+	public int getDelay(){
+		return _delay;
+	}
+	
+	public void setDelay(int delay){
+		if (delay < 0 )
+			throw new UiException("Illegal delay: "+delay+". Range: 0 ~ ");
+		
+		if(_delay!=delay){
+			_delay = delay;
+			smartUpdate("delay",_delay);
+		}	
+	}
+
+	
+	public boolean getDisabled() {
+		return _disabled;
+	}
+	
+	public void setDisabled(boolean disabled) {
+		if(_disabled != disabled){
+			_disabled = disabled;
+			smartUpdate("disabled", _disabled);					
+		}
+	}	
+		
 	public String getSelected(){
 		return _selected;
 	}
@@ -42,33 +107,11 @@ public class Autocomplete extends XulElement {
 			smartUpdate("selected", _selected);
 		}
 	}
-	
-	public String[] getAvailableTags(){
-		return _availableTags;
-	}
-
-	public void setAvailableTags(String availableTags){
-		if (availableTags == null) availableTags = "";
-		String[] tags = (String[]) coerceFromString(availableTags);
-		setAvailableTags(tags);
-	}
-
-	protected
-	Object coerceFromString(String value){
-		String[] result=value.split(",");
-		return result;
-	}
-	
-	public void setAvailableTags(String[] availableTags){
-		if(!Objects.equals(_availableTags, availableTags)){
-			_availableTags = availableTags;
-			smartUpdate("availableTags", availableTags);
-		}				
-	}
-	
+		
 	@Override
 	public void service(org.zkoss.zk.au.AuRequest request, boolean everError) {
 		final String cmd = request.getCommand();
+		//TODO: define customized event?
 		if (cmd.equals(Events.ON_SELECTION)) {
 			SelectionEvent evt = SelectionEvent.getSelectionEvent(request);
 			_selected = evt.getSelectedText(); 
@@ -83,11 +126,17 @@ public class Autocomplete extends XulElement {
 	throws java.io.IOException {
 		super.renderProperties(renderer);
 			
-		if(!_selected.equals(""))
-			render(renderer, "selected", _selected);
+		if(_disabled)
+			render(renderer, "disabled", _disabled);
 
-		if(_availableTags.length > 0)
-			render(renderer, "availableTags", _availableTags);
+		if(_delay != 300)
+			render(renderer, "delay", _delay);
 
+		if(_minLength != 1)
+			render(renderer, "minLength", _minLength);
+
+		if(_source != null && _source.isModified()){				
+			render(renderer, "source", _source.getValue());
+		}		
 	}		
 }
