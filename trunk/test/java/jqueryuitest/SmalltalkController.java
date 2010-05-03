c0873b72869191048728bf2c2c9ea9eb225fdb4a
whkuoattaiwan <whkuoattaiwan@641bb3ea-ff26-834b-be61-8224bbf0acb5>
2010-05-03 03:09:46 +0000

    testing

---
+++ trunk/test/java/jqueryuitest/SmalltalkController.java
@@ -0,0 +1,24 @@
+package jqueryuitest;
+
+import org.zkoss.jquery4j.jqueryui.button.Button;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+public class SmalltalkController extends GenericForwardComposer {
+		
+	Button target;
+	Button radio;
+	Button checkbox;
+
+	public void onClick$target(Event e) {
+		System.out.println("target clicked");
+	}
+
+	public void onClick$radio(Event e) {
+		System.out.println("radio clicked");
+	}
+
+	public void onClick$checkbox(Event e) {
+		System.out.println("checkbox clicked");
+	}
+}
